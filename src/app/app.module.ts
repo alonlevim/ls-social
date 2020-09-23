68e9670082c982a8ba3f59baa4177cebadc56846
Alon <alonlevim@gmail.com>
2020-09-24 00:22:23 +0300

    Add login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginRegistrationComponent } from './login-registration/login-registration.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      LoginRegistrationComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginRegistrationComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login-registration/login-registration.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-registration',
+  templateUrl: './login-registration.component.html',
+  styleUrls: ['./login-registration.component.scss']
+})
+export class LoginRegistrationComponent implements OnInit {
+  typeMethod = 'sign-up';
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
