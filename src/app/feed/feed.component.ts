0fd73021971a4b2c378facdbb39dca852abe0c05
Alon <alonlevim@gmail.com>
2020-09-24 19:34:48 +0300

    Add feed service

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FeedService } from '../feed.service';
+
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent implements OnInit {
+  posts = [];
 
-  constructor() { }
+  constructor(private feedService: FeedService) { }
 
   ngOnInit() {
+    this.posts = this.feedService.getPosts();
   }
 
 }
