f16685fef8473ebdaaf20abdb3ce74e3c702ccb6
Alon <alonlevim@gmail.com>
2020-09-24 23:11:28 +0300

    Add cors to package

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 const express           = require('express');
 const bodyParser        = require('body-parser');
+const cors              = require('cors');
 const mainRouter        = require('./router');
 
 const port = process.env.PORT || 8081;
 
 const app = express();
+
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
