b2325270988155fa6a6a998520e4d9e22703d950
Alon <alonlevim@gmail.com>
2020-09-24 21:23:12 +0300

    Add router

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
-const express           = require('express')
-const bodyParser        = require('body-parser')
+const express           = require('express');
+const bodyParser        = require('body-parser');
+const mainRouter        = require('./router');
 
 const port = process.env.PORT || 8081;
 
@@ -7,6 +8,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Router
+const router = express.Router();
+app.use('/api', mainRouter(router));
 
 // Start the server
 app.listen(port);
