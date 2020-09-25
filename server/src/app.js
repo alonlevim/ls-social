babae3eed9a6516b6436060a9db4d8761b095149
Alon <alonlevim@gmail.com>
2020-09-25 17:48:09 +0300

    Add mongoDB

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express           = require('express');
 const bodyParser        = require('body-parser');
 const cors              = require('cors');
+const mongoose          = require('mongoose');
 const mainRouter        = require('./router');
 
 const port = process.env.PORT || 8081;
+const databaseName = 'ls-social';
 
 const app = express();
 
@@ -17,3 +19,6 @@ app.use('/api', mainRouter(router));
 
 // Start the server
 app.listen(port);
+
+// Connection to Database
+mongoose.connect(`mongodb://localhost/${databaseName}`, { useNewUrlParser: true, useUnifiedTopology: true });
