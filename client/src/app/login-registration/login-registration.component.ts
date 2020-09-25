76d034a9afcc1ee152bef6497e42ee231d161673
Alon <alonlevim@gmail.com>
2020-09-25 23:58:03 +0300

    Add validation sign-in sign-up

--- client/src/app/login-registration/login-registration.component.ts
+++ client/src/app/login-registration/login-registration.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+export class User {
+  public name: string;
+  public email: string;
+  public password: string;
+}
+
 @Component({
   selector: 'app-login-registration',
   templateUrl: './login-registration.component.html',
   styleUrls: ['./login-registration.component.scss']
 })
 export class LoginRegistrationComponent implements OnInit {
+  model = new User();
   typeMethod = 'sign-up';
   
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: User) {
+    if( this.typeMethod === "sign-up" ) {
+
+    }
+    else if (this.typeMethod === "sign-in") {
+      
+    }
   }
 
 }
