30de59b8fc9535fc3735f42c1e402d820176b1cb
Alon <alonlevim@gmail.com>
2020-09-25 18:22:18 +0300

    Add verification

--- server/models/authentication.js
+++ server/models/authentication.js
@@ -1,7 +1,31 @@
 const User = require('../schemas/user');
 
+const succeededStatus = { status: "ok" };
+const failedStatus = { status: "failed" };
+
 module.exports = {
     addUser: (req, res) => {
+        // Verification
+        if (
+            typeof req.body.name === "undefined"
+            ||
+            req.body.name.trim() == ""
+            ||
+            typeof req.body.email === "undefined"
+            ||
+            req.body.email.trim() == ""
+            ||
+            !req.body.email.includes('@')
+            ||
+            typeof req.body.password === "undefined"
+            ||
+            req.body.password.trim() == ""
+            ||
+            req.body.password.length < 4
+        ) {
+            return res.json(failedStatus);
+        }
+        
         // Create user
         const newUser = new User({
             name: req.body.name,
@@ -10,10 +34,10 @@ module.exports = {
         });
 
         // Save and return status
-        newUser.save().then(()=>{
-            res.json({status: "ok"});
-        }).catch(()=>{
-            res.json({status: "failed"});
+        newUser.save().then(() => {
+            res.json(succeededStatus);
+        }).catch(() => {
+            res.json(failedStatus);
         });
     }
 };
