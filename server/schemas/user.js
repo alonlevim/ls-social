76aa29f8ae1e73a94c92b9ca09d17648c8acb3fc
Alon <alonlevim@gmail.com>
2020-09-26 13:22:22 +0300

    Add trim

--- server/schemas/user.js
+++ server/schemas/user.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: { type: String, required: true },
-    email: { type: String, require: true },
+    name: { type: String, required: true, trim: true },
+    email: { type: String, require: true, trim: true },
     password: { type: String, required: true }
 });
 
